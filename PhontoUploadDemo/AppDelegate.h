4fc4db320faa0b9709ea984c88b9b7f04166b9fc
apple <apple@appledeMacBook-Pro.local>
2015-08-26 10:34:12 +0800

    Initial Commit

---
+++ PhontoUploadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhontoUploadDemo
+//
+//  Created by apple on 15/8/26.
+//  Copyright (c) 2015年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhontoUploadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhontoUploadDemo
+//
+//  Created by apple on 15/8/26.
+//  Copyright (c) 2015年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
